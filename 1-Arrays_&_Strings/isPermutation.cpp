467eb3ff61cdcb107e77936f47c68f6e38b97890
NikTRSK <trajkovs@usc.com>
2016-10-07 23:39:24 -0700

    isPermutation

---
+++ 1-Arrays_&_Strings/isPermutation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isPermutaionBySorting(string str1, string str2) {
+  if (str1.length() != str2.length())
+    return false;
+
+  // Sort the two strings
+  std::sort(str1.begin(), str1.end());
+  std::sort(str2.begin(), str2.end());
+}
+
+int main(int argc, char *argv[]) {
+  char str[] = "ABCD";
+  int n = strlen(str);
+  char substring[] = "BAC";
+  bool check = permute(str, 0, n-1, substring);
+  cout << std::boolalpha << check << endl;
+}
